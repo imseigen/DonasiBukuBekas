3d752442687338e3731720805071171fa660ab8f
imseigen <seigenjane@gmail.com>
2024-11-15 14:33:28 +0700

    Update Donasi.java

--- src/main/java/com/mycompany/donasibukubekas/Donasi.java
+++ src/main/java/com/mycompany/donasibukubekas/Donasi.java
@@ -2,31 +2,5 @@ package com.mycompany.donasibukubekas;
 import java.util.Date;
 
 public class Donasi {
-    private String idDonasi;
-    private Donatur donatur;
-    private Buku buku;
-    private Date tanggal;
-
-    public Donasi(Donatur donatur, Buku buku) {
-        this.idDonasi = "D" + System.currentTimeMillis();
-        this.donatur = donatur;
-        this.buku = buku;
-        this.tanggal = new Date();
-    }
-    
-    public String getIdDonasi(){
-        return idDonasi;
-    }
-
-    public Donatur getDonatur() {
-        return donatur;
-    }
-
-    public Buku getBuku() {
-        return buku;
-    }
-
-    public Date getTanggal() {
-        return tanggal;
-    }
+  
 }
