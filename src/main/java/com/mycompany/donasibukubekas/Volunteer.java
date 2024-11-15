31652c499c2a7856a1dc2244a8fa5d9ff1f8b5b2
imseigen <seigenjane@gmail.com>
2024-11-15 14:35:05 +0700

    Update Volunteer.java

--- src/main/java/com/mycompany/donasibukubekas/Volunteer.java
+++ src/main/java/com/mycompany/donasibukubekas/Volunteer.java
@@ -2,22 +2,7 @@ package com.mycompany.donasibukubekas;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Volunteer extends User {
-    private List<Distribusi> tugas;
-    
-    public Volunteer(String idUser, String nama, String email, String password, int kontak, String alamat) {
-        super(idUser, nama, email, password, kontak, alamat);
-        this.tugas = new ArrayList<>();
-    }
-    
-    public void kelolaDistribusi(Donasi donasi){
-        Distribusi distribusi = new Distribusi(donasi);
-        tugas.add(distribusi);
-        System.out.println("Distribusi donasi berhasil ditambahkan");
-    }
-    
-    public List<Distribusi> getTugas(){
-        return tugas;
-    }
+public class Volunteer  {
+   
     
 }
