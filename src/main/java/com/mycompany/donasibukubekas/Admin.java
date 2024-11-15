8d4813b14c9cdc9f9b6296beba99a6e99cbd0d04
imseigen <seigenjane@gmail.com>
2024-11-15 14:32:13 +0700

    Update Admin.java

--- src/main/java/com/mycompany/donasibukubekas/Admin.java
+++ src/main/java/com/mycompany/donasibukubekas/Admin.java
@@ -1,18 +1,7 @@
 package com.mycompany.donasibukubekas;
 
-public class Admin extends User {
+public class Admin  {
 
-    public Admin(String idUser, String nama, String email, String password, int kontak, String alamat) {
-        super(idUser, nama, email, password, kontak, alamat);
-    }
-    
-    public void hapusPengguna(String email){
-        if (User.getUserDatabase().containsKey(email)){
-            User.getUserDatabase().remove(email);
-            System.out.println("User dengan email " + email + "telah dihapus");
-        } else {
-            System.out.println("User tidak ditemukan");
-        }
-    }
+ 
     
 }
